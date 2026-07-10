5e2e512a12ad747fa5824e1fe1e2e4e723ef2575
Morzkat <felix.david21@outlook.com>
2026-07-10 19:03:39 -0400

    Bootstrap project scaffold

--- src/utils/nameUtils.ts
+++
@@ -1,9 +0,0 @@
-export function countAs(value: string): number {
-  return [...value].reduce((count, character) => {
-    return character.toLowerCase() === "a" ? count + 1 : count;
-  }, 0);
-}
-
-export function hasExactlyThreeAs(name: string): boolean {
-  return countAs(name) === 3;
-}
